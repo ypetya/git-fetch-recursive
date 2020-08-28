d4f5d7f6f2eca111eb383a59e355309a6dd6dffe
Peter Kiss <ypetya@gmail.com>
2020-08-28 07:51:30 +0200

    fmt

--- git-fetch-recursive.go
+++ git-fetch-recursive.go
@@ -1,63 +1,62 @@
 package main
 
 import (
-"os"
-"os/exec"
-"sync"
-"fmt"
-w "./walkdir"
+	w "./walkdir"
+	"fmt"
+	"os"
+	"os/exec"
+	"sync"
 )
 
 func main() {
-  if len(os.Args) == 2 {
-    // walk -> git fetch
-    ch, done := make(chan string), make(chan bool)
-    go collect(ch,done)
+	if len(os.Args) == 2 {
+		// walk -> git fetch
+		ch, done := make(chan string), make(chan bool)
+		go collect(ch, done)
 
-    w.WalkDir(os.Args[1],ch, exclude)
-    close(ch)
-    <-done
-  } else {
-    //help()
-  }
+		w.WalkDir(os.Args[1], ch, exclude)
+		close(ch)
+		<-done
+	} else {
+		//help()
+	}
 }
 
-func exclude(relative string,absolute string) bool {
-  if relative[0] == '.' || relative == "node_modules" ||
-    relative == "build" || relative == "dist" || relative == "src" ||
-    relative == "test" || relative == "Pictures" {
-    return true
-  }
-  return false
+func exclude(relative string, absolute string) bool {
+	if relative[0] == '.' || relative == "node_modules" ||
+		relative == "build" || relative == "dist" || relative == "src" ||
+		relative == "test" || relative == "Pictures" {
+		return true
+	}
+	return false
 }
-
 
 func collect(dir <-chan string, done chan<- bool) {
-  var count int
-  waitAll := sync.WaitGroup{}
+	var count int
+	waitAll := sync.WaitGroup{}
 
-  for d := range dir {
-    f, err := os.Open(d+"/.git")
-    if err == nil {
-      f.Close()
-      waitAll.Add(1)
-      go runGitFetch(d, &waitAll)
-      count++
-    }
-  }
-  waitAll.Wait()
-  fmt.Println("\nTotal git repositories:", count)
-  done<-true
+	for d := range dir {
+		f, err := os.Open(d + "/.git")
+		if err == nil {
+			f.Close()
+			waitAll.Add(1)
+			go runGitFetch(d, &waitAll)
+			count++
+		}
+	}
+	waitAll.Wait()
+	fmt.Println("\nTotal git repositories:", count)
+	done <- true
 }
 
 func runGitFetch(dir string, w *sync.WaitGroup) {
 
-  cmd:=exec.Command("git","fetch","--all")
-  cmd.Dir = dir
-  if cmd.Run() == nil {
-    fmt.Print("+")
-  } else {
-    fmt.Print("-")
-  }
-  (*w).Done()
+	cmd := exec.Command("git", "fetch", "--all")
+	cmd.Dir = dir
+	if cmd.Run() == nil {
+		fmt.Print("+")
+	} else {
+		fmt.Print("-")
+	}
+	(*w).Done()
 }
--- walkdir/walkdir.go
+++ walkdir/walkdir.go
@@ -1,32 +1,32 @@
 package walkdir
 
-import(
-"os"
+import (
+	"os"
 )
 
 func WalkDir(folder string,
-  visit chan<- string,
-  isExcluded func(string,string) bool) error {
+	visit chan<- string,
+	isExcluded func(string, string) bool) error {
 
 	f, err := os.Open(folder)
 	if err != nil {
 		return err
 	}
 
-  visit<-folder
+	visit <- folder
 
-  list, err := f.Readdir(-1)
+	list, err := f.Readdir(-1)
 	f.Close()
 	for _, fileInfo := range list {
 		if fileInfo.IsDir() {
-      folderName := fileInfo.Name()
-      fN := folder+"/"+folderName
-      if ! isExcluded(folderName,fN) {
-        err := WalkDir(fN, visit, isExcluded)
-        if err != nil {
-          return err
-        }
-      }
+			folderName := fileInfo.Name()
+			fN := folder + "/" + folderName
+			if !isExcluded(folderName, fN) {
+				err := WalkDir(fN, visit, isExcluded)
+				if err != nil {
+					return err
+				}
+			}
 		}
 	}
 	if err != nil {
